7ab4af861e73054c5033e2ec6f31d8d4158d1fdd
HyeonjinChoi <eaglushyeonjin@gmail.com>
2024-05-30 20:37:07 +0900

    init commit

---
+++ src/test/java/com/sparta/schedulemanagementserver/ScheduleManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.schedulemanagementserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
